99fd47b45a7d892916fb6c5de994ef9edf8d24f2
shinithp <shinithp78@gmail.com>
2025-09-17 23:07:22 +0530

    add resume download

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -27,11 +27,13 @@ export const Home = () => {
             </a>
 
             <a
-              href="#"
+              href="/resume.pdf" 
+              download="Shinith-Resume.pdf"
               className="flex justify-center items-center gap-2 border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
              hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
             >
-             <Download className="size-5"/>My Resume
+              <Download className="size-5" />
+              My Resume
             </a>
           </div>
         </div>
